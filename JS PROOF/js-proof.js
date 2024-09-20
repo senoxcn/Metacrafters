92b57938ee67224fe89eeaa7dc314689f129ac56
Xeon <angecdmt@gmail.com>
2024-09-21 02:23:57 +0800

    assessment

--- JS PROOF/js-proof.js
+++ JS PROOF/js-proof.js
@@ -9,24 +9,40 @@ Assessment Requirements
 */
 
 // create a variable to hold your NFT's
+const MY_NFT = [];
 
 // this function will take in some values as parameters, create an
 // NFT object using the parameters passed to it for its metadata,
 // and store it in the variable above.
-function mintNFT() {
 
+
+function mintNFT(name, value, rarity, color) {
+   const info = {
+      "name": name,
+      "value": value,
+      "rarity": rarity,
+      "color": color
+   }
+   MY_NFT.push(info);
 }
 
 // create a "loop" that will go through an "array" of NFT's
 // and print their metadata with console.log()
 function listNFTs() {
-
+   MY_NFT.forEach(nft => {
+      console.log(`${nft.name}: \n ${nft.value} \n ${nft.rarity} \n ${nft.color} \n`)
+   });
 }
 
 // print the total number of NFTs we have minted to the console
 function getTotalSupply() {
-
+   console.log("\n Total: " + MY_NFT.length);
 }
 
 // call your functions below this line
-
+mintNFT("Primogems", 1600, "5 star", "White");
+mintNFT("Mora", 500, "3 star", "Yellow");
+mintNFT("Fates", 1000, "4 star", "Pink");
+mintNFT("Crystals", 100, "1 star", "Blue");
+listNFTs();
+getTotalSupply();
