9d0d92ac070a99f8405ef707b371a4a3132cc99b
xe on <108950763+senoxcn@users.noreply.github.com>
2024-12-02 16:49:14 +0800

    assessment

---
+++ Blood Shard/BloodShard.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/access/Ownable.sol";
+
+contract BloodShard is ERC20, Ownable {
+    // Constructor to set the token name, symbol, and initial owner
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        //Ownable constructor doesn't take parameters and is automatically set to the contract deployer
+        
+    }
+
+    // Mint function - only callable by the contract owner
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    // Burn function - allows any user to burn their own tokens
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+
+    // Override decimals to set a standard 18 decimal places
+    function decimals() public pure override returns (uint8) {
+        return 18;
+    }
+}
